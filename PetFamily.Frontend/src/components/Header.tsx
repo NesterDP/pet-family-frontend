0ab09830533f895a09d3b8e24a8cd711f21b3967
NesterDP <s-dolg_96@mail.ru>
2025-05-07 01:17:01 +0300

    Changed header, added icons and buttons to it

--- PetFamily.Frontend/src/components/Header.tsx
+++ PetFamily.Frontend/src/components/Header.tsx
@@ -1,19 +1,48 @@
-import { AppBar, Button } from "@mui/material";
-import { NavLink } from "react-router";
+import CrueltyFreeIcon from "@mui/icons-material/CrueltyFree";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PersonIcon from "@mui/icons-material/Person";
+import StarIcon from "@mui/icons-material/Star";
+import { AppBar, IconButton } from "@mui/material";
+import { useNavigate } from "react-router";
 
 export function Header() {
+	const navigate = useNavigate();
+
 	return (
 		<AppBar position="static">
 			<div className="flex flex-row items-center justify-between py-2 px-3">
-				<div className="flex flex-row items-center gap-4 justfy-center">
-					<NavLink to="/" className="sm:text-2xl pr-5">
-						PetFamily
-					</NavLink>
-					<NavLink to="game">Игра</NavLink>
-					<NavLink to="volunteers">Волонтеры</NavLink>
-					<NavLink to="pets">Питомцы</NavLink>
+				{/* Левые иконки */}
+				<div className="flex flex-row items-center gap-2">
+					<IconButton
+						onClick={() => navigate("/")}
+						color="inherit"
+						edge="start"
+					>
+						<CrueltyFreeIcon />
+					</IconButton>
+					<IconButton onClick={() => navigate("/favorite")} color="inherit">
+						<StarIcon />
+					</IconButton>
 				</div>
-				<Button color="inherit">Login</Button>
+
+				{/* Центральный текст */}
+				<div className="text-2xl font-medium mx-auto cursor-default">
+					PetFamily
+				</div>
+
+				{/* Правые иконки */}
+				<div className="flex flex-row items-center gap-2">
+					<IconButton onClick={() => navigate("/account")} color="inherit">
+						<PersonIcon />
+					</IconButton>
+					<IconButton
+						onClick={() => navigate("/logout")}
+						color="inherit"
+						edge="end"
+					>
+						<LogoutIcon />
+					</IconButton>
+				</div>
 			</div>
 		</AppBar>
 	);
